d78bbd090d3f7b80fe565e3416de2b1cee6c2356
shangmingyang <shang_ming_yang@163.com>
2017-10-25 19:30:12 +0800

    update gitignore to ignore idea

--- run_train.sh
+++ run_train.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-source config
-THEANO_FLAGS=floatX=float32,device=cpu python src/linear_ae_ff_stable.py --num-epoches 500 --update-times-g2d 10 --batch-size 256 --alt-loss --joint-dim 128 --train --recon-weight 0 > $reconWeight.log
+THEANO_FLAGS=floatX=float32,device=cpu python src/linear_ae_ff_stable.py --num-epoches 10000 --update-times-g2d 10 --batch-size 256 --alt-loss --joint-dim 128 --train --recon-weight 0 > $reconWeight.log
 
 #cut -d ' ' -f 1 data/$config/vocab-freq.$lang1 > data/$config/vocab.$lang1
 #python3 scripts/translate.py data/$config/transformed-1.$lang1 data/$config/word2vec.$lang2 data/$config/vocab.$lang1 data/$config/result.1
